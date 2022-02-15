e0e62c65e0d8e605682c030e27aa49e29b44644f
vaishnavipasupulati <98844705+vaishnavipasupulati@users.noreply.github.com>
2022-02-15 11:16:08 -0500

    Update pypart.py

--- pypart.py
+++ pypart.py
@@ -1,1 +1,12 @@
-print("HELLO WORLD")
+# Python 3.x code to demonstrate star pattern
+
+# Function to demonstrate printing pattern
+def pypart(n):
+	myList = []
+	for i in range(1,n+1):
+		myList.append("*"*i)
+	print("\n".join(myList))
+
+# Driver Code
+n = 5
+pypart(n)
